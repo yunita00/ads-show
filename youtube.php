dfb4ab1a96100069707514778a4a914048a0f1d7
trezee <63031302+trezee29@users.noreply.github.com>
2022-04-09 10:43:04 +0700

    Create youtube.php

---
+++ youtube.php
@@ -0,0 +1,67 @@
+<?php
+ $link ="https://youtube.com/shorts/oiiHrGvpXLE?feature=share";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <title>Ads Preview</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="ads, advertising, public, publisher, earn money by ads, money, online earning,">
+ <meta name="author" content="Trezee">
+
+ <!-- Bootstrap style --> 
+ <link id="callCss" rel="stylesheet" href="themes/css/bootstrap.min.css" media="screen"/>
+ <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+ <!-- Bootstrap style responsive -->	
+ <link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+ <link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+
+ <!-- fav and touch icons -->
+ <link rel="shortcut icon" href="themes/images/ico/favicon.ico">
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5809534288477392"
+     crossorigin="anonymous"></script>
+ 
+ <!-- za.gl -->
+  <script>
+    function pageRedirect() {
+        window.open("https://trezee29.github.io/ads-show/youtube.html");
+    }      
+    setTimeout("pageRedirect()", 10000);
+ </script>
+</head>
+<body>
+
+ <div id="mainBody">
+  <div class="container" align="center">
+
+   <div class="row">
+    <div class="span12">
+     <iframe data-aa="1596652" src="//acceptable.a-ads.com/1596652" style="border:0px; padding:0; width:728px; height:90px; overflow:hidden; background-color: transparent;" ></iframe>
+    </div>
+   </div><br>
+
+   <div class="row">
+    <div class="span4">
+     <iframe src="<?= $link; ?>" style="border:0px; padding:0; width:125px; height:250px; overflow:hidden; background-color: transparent; autoplay" ></iframe>
+    </div>
+    <div class="span4">
+     <iframe src="<?= $link; ?>" style="border:0px; padding:0; width:125px; height:250px; overflow:hidden; background-color: transparent; autoplay" ></iframe>
+    </div>
+    <div class="span4">
+     <iframe src="<?= $link; ?>" style="border:0px; padding:0; width:125px; height:250px; overflow:hidden; background-color: transparent; autoplay" ></iframe>
+    </div>
+   </div><br>
+
+   <div class="row">
+    <div class="span12">
+     <iframe data-aa="1596652" src="//acceptable.a-ads.com/1596652" style="border:0px; padding:0; width:728px; height:90px; overflow:hidden; background-color: transparent;" ></iframe>
+    </div>
+   </div><br>
+
+  </div>
+ </div>
+
+</body>
+</html>
